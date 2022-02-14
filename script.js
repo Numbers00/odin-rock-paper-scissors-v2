60189b03127e57bd8c89a49dff01e2eb128b7beb
Numbers_00 <kylemariangelo@gmail.com>
2022-02-14 09:17:57 +0800

    added game intro text

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-let selections = ['rock', 'paper', 'scissors']
+// let selections = ['rock', 'paper', 'scissors']
 
-let playerSelection = ''
-let computerSelection = ''
+// let playerSelection = ''
+// let computerSelection = ''
 
-let playerScore = 0;
-let computerScore = 0;
+// let playerScore = 0;
+// let computerScore = 0;
 
-window.onload = game
+// window.onload = game
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt('Rock, Paper, Scissors! Pick one! (Input nothing to get random)').trim().toLowerCase() || playerPlay()
-        computerSelection = computerPlay()
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         playerSelection = prompt('Rock, Paper, Scissors! Pick one! (Input nothing to get random)').trim().toLowerCase() || playerPlay()
+//         computerSelection = computerPlay()
 
-        let result = playRound(playerSelection, computerSelection)
-        console.log(result)
+//         let result = playRound(playerSelection, computerSelection)
+//         console.log(result)
 
-        result.includes('Win') ? playerScore++ : 
-            result.includes('Lose') ? computerScore++ : ''
-    }
+//         result.includes('Win') ? playerScore++ : 
+//             result.includes('Lose') ? computerScore++ : ''
+//     }
 
-    console.log(`Player: ${playerScore} vs. Computer: ${computerScore}`)
-    if (playerScore > computerScore) console.log('Player Won!')
-    else if (playerScore < computerScore) console.log('Computer Won!')
-    else console.log('It\'s a Tie!')
-}
+//     console.log(`Player: ${playerScore} vs. Computer: ${computerScore}`)
+//     if (playerScore > computerScore) console.log('Player Won!')
+//     else if (playerScore < computerScore) console.log('Computer Won!')
+//     else console.log('It\'s a Tie!')
+// }
 
-function playRound(playerSelection, computerSelection) {
-    let result = ''
+// function playRound(playerSelection, computerSelection) {
+//     let result = ''
 
-    switch (playerSelection) {
-        case 'rock':
-            computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
-                computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
-                    result = 'You Win! Rock beats Scissors!'
-            break
-        case 'paper':
-            computerSelection === 'rock' ? result = 'You Win! Paper beats Rock!' :
-                computerSelection === 'paper' ? result = 'It\s a Tie! You\'re both Papers!' :
-                    result = 'You Lose! Scissors beats Paper!'
-            break
-        case 'scissors':
-            computerSelection === 'rock' ? result = 'You Lose! Rock beats Scissors!' :
-                computerSelection === 'paper' ? result = 'You Win! Scissors beats Paper!' :
-                    result = 'It\'s a Tie! You\'re both Scissors!'
-            break
-        default:
-            computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
-                computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
-                    result = 'You Win! Rock beats Scissors!'
-    }
+//     switch (playerSelection) {
+//         case 'rock':
+//             computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
+//                 computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
+//                     result = 'You Win! Rock beats Scissors!'
+//             break
+//         case 'paper':
+//             computerSelection === 'rock' ? result = 'You Win! Paper beats Rock!' :
+//                 computerSelection === 'paper' ? result = 'It\s a Tie! You\'re both Papers!' :
+//                     result = 'You Lose! Scissors beats Paper!'
+//             break
+//         case 'scissors':
+//             computerSelection === 'rock' ? result = 'You Lose! Rock beats Scissors!' :
+//                 computerSelection === 'paper' ? result = 'You Win! Scissors beats Paper!' :
+//                     result = 'It\'s a Tie! You\'re both Scissors!'
+//             break
+//         default:
+//             computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
+//                 computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
+//                     result = 'You Win! Rock beats Scissors!'
+//     }
 
-    return result
-}
+//     return result
+// }
 
-function playerPlay() {
-    return selections[Math.floor(Math.random() * 3)]
-}
+// function playerPlay() {
+//     return selections[Math.floor(Math.random() * 3)]
+// }
 
-function computerPlay() {
-    return selections[Math.floor(Math.random() * 3)]
-}
+// function computerPlay() {
+//     return selections[Math.floor(Math.random() * 3)]
+// }
--- script2.js
+++ script2.js
@@ -16,25 +16,25 @@ function playRound(playerSelection, computerSelection) {
   let computerSelection = computerPlay();
 
   switch (playerSelection) {
-      case 'rock':
-          computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
-              computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
-                  result = 'You Win! Rock beats Scissors!';
-          break;
-      case 'paper':
-          computerSelection === 'rock' ? result = 'You Win! Paper beats Rock!' :
-              computerSelection === 'paper' ? result = 'It\s a Tie! You\'re both Papers!' :
-                  result = 'You Lose! Scissors beats Paper!';
-          break;
-      case 'scissors':
-          computerSelection === 'rock' ? result = 'You Lose! Rock beats Scissors!' :
-              computerSelection === 'paper' ? result = 'You Win! Scissors beats Paper!' :
-                  result = 'It\'s a Tie! You\'re both Scissors!';
-          break;
-      default:
-          computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
-              computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
-                  result = 'You Win! Rock beats Scissors!';
+    case 'rock':
+      computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
+          computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
+              result = 'You Win! Rock beats Scissors!';
+      break;
+    case 'paper':
+      computerSelection === 'rock' ? result = 'You Win! Paper beats Rock!' :
+          computerSelection === 'paper' ? result = 'It\s a Tie! You\'re both Papers!' :
+              result = 'You Lose! Scissors beats Paper!';
+      break;
+    case 'scissors':
+      computerSelection === 'rock' ? result = 'You Lose! Rock beats Scissors!' :
+          computerSelection === 'paper' ? result = 'You Win! Scissors beats Paper!' :
+              result = 'It\'s a Tie! You\'re both Scissors!';
+      break;
+    default:
+      computerSelection === 'rock' ? result = 'It\'s a Tie! You\'re both Rocks!' :
+          computerSelection === 'paper' ? result = 'You Lose! Paper beats Rock!' :
+              result = 'You Win! Rock beats Scissors!';
   }
 
   return result;
