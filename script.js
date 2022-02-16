38ce3c2fc45b3dc2cfad598267c2b011a13d427e
Numbers_00 <kylemariangelo@gmail.com>
2022-02-16 15:01:29 +0800

    fixed matching phase

--- script.js
+++ script.js
@@ -14,25 +14,26 @@ window.addEventListener('keydown', (e) => {
   }
 });
 
+// Credits: https://stackoverflow.com/a/53452241
 function wait(ms) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("Done waiting");
       resolve(ms)
-    }, ms )
+    }, ms)
   })
 }
 
 let gamerNamer = require('gamer-namer');
 let epithetGiver = require('epithet');
 
+let names = [];
+let epithets = [];
+let images = [];
+
 window.onload = () => {
   document.getElementById('name').placeholder = 'Name: ' + gamerNamer.generateName();
   document.getElementById('epithet').placeholder = 'Epithet: ' + epithetGiver.choose();
 }
-
-let names = [];
-let epithets = [];
 
 document.getElementById('input-container').addEventListener('keydown', e => {
   if (e.keyCode === 13) {
@@ -54,7 +55,7 @@ function addAvatarCards(index) {
   avatarCard.classList.add("avatar-card");
 
   const avatarImg = document.createElement('img');
-  avatarImg.src = 'imgs/template_avatar.png';
+  avatarImg.src = images[index];
   avatarImg.alt = 'Avatar';
   avatarCard.appendChild(avatarImg);
 
@@ -62,11 +63,12 @@ function addAvatarCards(index) {
   textContainer.classList.add('text-container');
 
   const h4 = document.createElement('h4');
+  h4.style.margin = 2;
   h4.textContent = names[index];
-  h4.fontWeight = 700;
   textContainer.appendChild(h4);
 
   const para = document.createElement('p');
+  para.style.margin = 2;
   para.textContent = epithets[index];
   textContainer.appendChild(para);
 
@@ -86,8 +88,27 @@ function addAvatarCards(index) {
 async function startMatching() {
   document.getElementsByClassName('avatar-card')[0].remove();
 
+  let picsumImages = [];
+
+  // Credits: https://stackoverflow.com/a/12460434
+  await $.getJSON('https://picsum.photos/v2/list', (data) => {
+    for (let i = 0; i < data.length; i++) {
+      picsumImages.push(data[i]['download_url']);
+    }
+  });
+
+  for (let i = 0, j = 0; i < 64; i++) {
+    if (j <= 29) {
+      if (Math.floor(Math.random() * 3) !== 0) {
+        images.push(picsumImages[j]);
+        j++;
+      } else images.push('imgs/template_avatar.png');
+    } else images.push('imgs/template_avatar.png');
+  }
+
   addAvatarCards(0);
 
+  // # of joined gladiators in the matching phase will start at this number
   let randNum = Math.floor(Math.random() * 23) + 25;
 
   for (let i = 1; i < randNum; i++) {
@@ -118,6 +139,7 @@ async function startMatching() {
     }
     addAvatarCards(i);
 
+    // at times, more than 1 new gladiators will join
     if (Math.floor((Math.random() * 3) + 1) % 2 != 0) await wait(Math.random() * 400);
 
     document.getElementById('bottom-div-title').textContent = `Waiting for Other Players... (${names.length}/64)`;
@@ -129,7 +151,7 @@ async function startMatching() {
       document.getElementById('bottom-div-title').textContent = 'Starting Game...';
     } else document.getElementById('bottom-div-title').textContent = 'Starting Game..';
 
-    if (i === 5) startGame();
+    //if (i === 5) startGame();
   }
 }
 
